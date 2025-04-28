08f9d8856eedc0d89de83ac191f91d64a95fc967
shubhayansarkar <sarkarsar123gmail.com>
2025-04-28 20:48:57 +0530

    correcte the filepaths

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,7 +1,7 @@
 from src.ocr_func import ocr_pdf, ocr_image
 
 def test_ocr_image():
-    assert ocr_image(r"tests\samples\IMG_20250401_144638487.jpg") == 'Shubhayan Sarkar'
+    assert ocr_image(r"ocr_mlops\tests\samples\ex1.jpg") == 'Shubhayan Sarkar'
 
 def test_ocr_pdf():
-    assert ocr_pdf(r'test\samples\ex2.pdf') == 'Shubhayan Sarkar'
+    assert ocr_pdf(r'ocr_mlops\tests\samples\ex2.pdf') == 'Shubhayan Sarkar'
