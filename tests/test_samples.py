b7937f377a926082d4703a1ea43e393639f9a57a
shubhayansarkar <sarkarsar123gmail.com>
2025-04-28 22:25:32 +0530

    checks final

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -3,8 +3,8 @@ import os
 
 def test_ocr_image():
     image_path = os.path.join("tests", "samples", "ex1.jpg")
-    assert ocr_image(image_path) == 'Shubhayan Sarkar'
+    assert 'Shubhayan Sarkar' in ocr_image(image_path)
 
 def test_ocr_pdf():
     pdf_path = os.path.join("tests", "samples", "ex2.pdf")
-    assert ocr_pdf(pdf_path) == 'Shubhayan Sarkar'
+    assert 'Shubhayan Sarkar' in ocr_pdf(pdf_path)
